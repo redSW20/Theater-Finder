cc2f186abbfcda0cee42c255ba4964aa4cffbad1
rls5113 <stuartrl@comcast.net>
2019-03-12 19:49:09 -0500

    working ajax

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+function makeAjaxCall() {
+    // console.log("showPics ...");
+    // var param1 = $(this).attr("qryParam1").toLowerCase().replace(" ","+");
+    var limit = 10;
+    var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-03-13&zip=37027&api_key=mdd9zjstfnweg7aereckewe6";
+
+    console.log(qryURL);
+    
+    $.ajax({
+        url: qryURL,
+        method: "GET"
+    }).then( function(response) {
+        console.log(JSON.stringify(response));
+        $("results").text(JSON.stringify(response));
+    }); 
+
+}
+
+makeAjaxCall();
