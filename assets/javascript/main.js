aa08ed2fd28d81a2204a1ab68659067974f77058
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-03-13 22:01:21 -0500

    Search by zip and date functional and returning results in console log. Removed movie title, movie time and rating search fields. The api does not provide these search methods.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,39 +1,40 @@
 $('document').ready( function() {
 //variables to store user input from index.html input form
 var searchZip;
-// var searchCityState = $('#movieCityState');
-// var searchMovie = $('#movieTitle');
-// var searchDate = $('#movieDate');
-// var searchTimes = $('#movieTime');
-// var searchRating = $('#ratingSelector');
+// var searchCityState;
+// var searchMovie;
+var searchDate;
+// var searchTimes;
+// var searchRating;
 
 
 //on submit button on click call the api based on search parameters entered in form
-$('#submitBtn').on('click', function(event) {
-    event.preventDefault();
-    searchZip = $('#movieZIP').val().trim();
-    console.log(searchZip);
-    makeAjaxCall();
-});
+    $('#submitBtn').on('click', function(event) {
+        event.preventDefault();
+        searchZip = $('#movieZIP').val().trim();
+        searchDate = $('#movieDate').val().trim();
+        console.log(searchZip);
+        makeAjaxCall();
+        searchZip = $('#movieZIP').val('');
+    });
 
-function makeAjaxCall() {
-    // console.log("showPics ...");
-    // var param1 = $(this).attr("qryParam1").toLowerCase().replace(" ","+");
-    var limit = 10;
-    var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-03-13&zip=" + searchZip + "&api_key=mdd9zjstfnweg7aereckewe6";
+    //function to call api
+    function makeAjaxCall() {
+        // console.log("showPics ...");
+        // var param1 = $(this).attr("qryParam1").toLowerCase().replace(" ","+");
+        var limit = 10;
+        var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" + searchDate + "&zip=" + searchZip + "&api_key=mdd9zjstfnweg7aereckewe6";
 
-    console.log(qryURL);
-    
-    $.ajax({
-        url: qryURL,
-        method: "GET"
-    }).then( function(response) {
-        console.log(JSON.stringify(response));
-        $("results").text(JSON.stringify(response));
-    }); 
+        console.log(qryURL);
+        
+        $.ajax({
+            url: qryURL,
+            method: "GET"
+        }).then( function(response) {
+            console.log(JSON.stringify(response));
+            $("results").text(JSON.stringify(response));
+        }); 
 
-};
-
-// makeAjaxCall();
+    };
 
 }); 
