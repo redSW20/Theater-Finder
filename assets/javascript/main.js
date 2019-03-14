b5fe99bebc35804334008a585aba01ae3ed67c30
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-03-13 20:36:44 -0500

    Return search results based on zipcode field entry after clicking submit button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,26 @@
+$('document').ready( function() {
+//variables to store user input from index.html input form
+var searchZip;
+// var searchCityState = $('#movieCityState');
+// var searchMovie = $('#movieTitle');
+// var searchDate = $('#movieDate');
+// var searchTimes = $('#movieTime');
+// var searchRating = $('#ratingSelector');
+
+
+//on submit button on click call the api based on search parameters entered in form
+$('#submitBtn').on('click', function(event) {
+    event.preventDefault();
+    searchZip = $('#movieZIP').val().trim();
+    console.log(searchZip);
+    makeAjaxCall();
+});
 
 function makeAjaxCall() {
     // console.log("showPics ...");
     // var param1 = $(this).attr("qryParam1").toLowerCase().replace(" ","+");
     var limit = 10;
-    var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-03-13&zip=37027&api_key=mdd9zjstfnweg7aereckewe6";
+    var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-03-13&zip=" + searchZip + "&api_key=mdd9zjstfnweg7aereckewe6";
 
     console.log(qryURL);
     
@@ -15,7 +32,8 @@ function makeAjaxCall() {
         $("results").text(JSON.stringify(response));
     }); 
 
-}
+};
 
-makeAjaxCall();
+// makeAjaxCall();
 
+}); 
