495e13f76fda5f897d8e9d270f2e0936f546ca1e
RedSW20 <kevbailey1227@gmail.com>
2019-03-16 01:48:40 -0500

    fixed the ratings crashing issue and displayed them with other results

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
         // function makeAjaxCall() {
         var limit = 10;
-        var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-03-20&zip=37076&api_key=mdd9zjstfnweg7aereckewe6";
+        var qryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2019-03-20&zip=37076&api_key=xw86j6eejrw4z8npzsxjwexx";
         console.log(qryURL);
 
         $.ajax({
@@ -18,23 +18,29 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             $("results").text(JSON.stringify(response));
-            console.log(response[0]);   
+            console.log(response);   
             
 
 
             var table = $("#t1");
                    // <td>$<DELETE ME WITH AN IF STATEMENT TO PREVENT READ ERROR FROM LACK OF RATING CODE>{response[i].ratings.code}</td>
 
-            for (var i = 0; i < response.length; i++) {
+            for (var i = 0; i < 10; i++) {
                 var rT = response[i].runTime;
                 var rT = rT.substr(2);
                 console.log(rT);
-
+                var ratings = response[i].ratings[0].code;
+                var rating = "Not Rated"
+                if (ratings == null) {
+                    ratings = rating
+                };
+                console.log(ratings);
 
 
             table.append(
                 `<tr>
                 <th scope="row">${response[i].title}</th>
+                <td>${ratings}</td>
                 <td>${rT}</td>
                 <td>${response[i].genres}</td>
                 <td> INSERT SHOWTIMES HERE</td>
